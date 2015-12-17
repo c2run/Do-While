8f6def05fb4799ccaf02b542078698df8bb20f4a
C2RUN <jose.cerda66@gmail.com>
2015-12-17 14:42:54 -0300

    Primeros cambios
    Ejemplo del bucle Do While en PHP

---
+++ DoWhile.php
@@ -0,0 +1,16 @@
+<?php
+//Ejemplo DO WHILE bucle
+
+  $x = 1;
+
+  do{
+  //Imprima el texto concatenando la variable
+    echo "El número es: $x <br>";
+    $x++;
+    //incrementar la variable en 1
+  }
+  while($x <= 5);
+  //Mientras la variable sea inferior o igual a 5
+
+
+?>
